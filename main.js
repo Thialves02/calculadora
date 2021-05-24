e8fe4f24b9042f077ec1f00728cf727070390d4a
Thialves02 <82343110+Thialves02@users.noreply.github.com>
2021-05-24 11:05:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ function insert(num){
     var number = document.getElementById('text').innerHTML;
     document.getElementById('text').innerHTML = number + num;
 }
+
 function erase(){
     var result = document.getElementById('text').innerHTML;
     document.getElementById('text').innerHTML = result.substring(0, result.length -1);
 }
+
 function clean(){
     document.getElementById('text').innerHTML = "";
 }
