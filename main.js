245f11161a67e1cd1d6645958e3945bf13ada1d3
Thialves02 <82343110+Thialves02@users.noreply.github.com>
2021-05-24 11:02:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function insert(num){
+    var number = document.getElementById('text').innerHTML;
+    document.getElementById('text').innerHTML = number + num;
+}
+function erase(){
+    var result = document.getElementById('text').innerHTML;
+    document.getElementById('text').innerHTML = result.substring(0, result.length -1);
+}
+function clean(){
+    document.getElementById('text').innerHTML = "";
+}
+
+function calc(){
+    var result = document.getElementById('text').innerHTML;
+    if(result ==''){
+        document.getElementById('text').innerHTML= 'Put a value';
+    }else{
+    document.getElementById('text').innerHTML = eval(result);
+    }
+}
+
